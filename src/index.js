54f8f4ddb529968bbc8aeb403708e1c25e632604
Mark Williams <m.williams.3141@gmail.com>
2021-02-19 13:26:30 +0000

    Setup webpack

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import * as Blockly from 'blockly';
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     const workspace = Blockly.inject('blocklyDiv',
-//         {
-//             toolbox: document.getElementById('toolbox'),
-//             media: 'media/'
-//         });
-//
-//     const lang = 'JavaScript';
-//     const button = document.getElementById('blocklyButton');
-//     button.addEventListener('click', function () {
-//         alert("Check the console for the generated output.");
-//         const code = Blockly[lang].workspaceToCode(workspace);
-//         console.log(code);
-//     })
-// });
+document.addEventListener("DOMContentLoaded", function () {
+    const workspace = Blockly.inject('blocklyDiv',
+        {
+            toolbox: document.getElementById('toolbox'),
+            media: 'media/'
+        });
+});
